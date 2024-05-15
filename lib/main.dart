981ed177f455cbb81396102b2e35a6927fce121c
futuraflutter <fluttertrainer.futuralabs@gmail.com>
2024-05-15 10:06:08 +0530

    project started with color scheme

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_gallery/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(useMaterial3: true)
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      // home: const ,
+    );
+  }
+}
